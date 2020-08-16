46ddf31c35b70e9cd6adf036aedcd2213c12e33f
Glazynov A <glazunov_ay@lemz-t.ru>
2020-08-16 15:23:09 +0700

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
-import 'package:unsplash_app/views/main_gallery.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:unsplash_app/views/main_gallery_widget.dart';
+import 'package:unsplash_app/views/full_screen_widget.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(initialRoute: '/',
+      routes: {
+        '/': (context) => GalleryWidget(),
+        '/second': (context) => FullScreenWidget(),
+      }
+    );
+  }
+}
 
 void main() {
-  runApp(MyApp());
+  runApp(Application());
 }
