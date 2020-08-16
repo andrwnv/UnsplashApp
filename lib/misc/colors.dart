306f8576dd57df4fa5672fa997aef2d0e60be4fe
Glazynov A <glazunov_ay@lemz-t.ru>
2020-08-16 18:07:21 +0700

    add colors const

---
+++ lib/misc/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const PrimaryColor = Color(0xFF1F1F1F);
+  static const ImageCardColor = Color(0xFFF4F4F4);
+  static const BackgroundColor = Color(0xFFFFFFFF);
+
+  static const ErrorColor = Color(0xFFFF4F4F);
+  static const LoadingColor = Color(0xFF57FF54);
+}
